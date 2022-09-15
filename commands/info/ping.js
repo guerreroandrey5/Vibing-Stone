e7ac7c58ec4028d19ce0539bb752545e3a589ae1
guerreroandrey5 <guerreroandrey@gmail.com>
2022-09-14 22:30:36 -0600

    Updated Dependencies

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -4,7 +4,7 @@ module.exports = {
     description: "Show the Bot's ping",
     run: async (client, message, args, prefix) => {
         message.reply({
-            content: `:ping_pong: **PONG! Api Ping is: \`${client.ws.ping}ms\`**`
+            content: `:ping_pong: **PONG! My Ping is: \`${client.ws.ping}ms\`**`
         }).catch(() => null);
     },
 };
